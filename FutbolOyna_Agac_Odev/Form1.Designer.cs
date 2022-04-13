c9d0fee978a67087a57821f735c27d5c8faf7de9
oktaygulec <oktay.gulec@outlook.com>
2022-04-13 03:03:30 +0300

    Initial commit

---
+++ FutbolOyna_Agac_Odev/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace FutbolOyna_Agac_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbHavaDurumu = new System.Windows.Forms.ComboBox();
+            this.cbSicaklik = new System.Windows.Forms.ComboBox();
+            this.cbNem = new System.Windows.Forms.ComboBox();
+            this.cbRuzgar = new System.Windows.Forms.ComboBox();
+            this.cbSahaBoyutu = new System.Windows.Forms.ComboBox();
+            this.cbCimDurumu = new System.Windows.Forms.ComboBox();
+            this.lblFutbolOynamaDurumu = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFutbolOyna = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbHavaDurumu
+            // 
+            this.cbHavaDurumu.FormattingEnabled = true;
+            this.cbHavaDurumu.Location = new System.Drawing.Point(282, 152);
+            this.cbHavaDurumu.Name = "cbHavaDurumu";
+            this.cbHavaDurumu.Size = new System.Drawing.Size(121, 21);
+            this.cbHavaDurumu.TabIndex = 0;
+            this.cbHavaDurumu.SelectedValueChanged += new System.EventHandler(this.cbHavaDurumu_SelectedValueChanged);
+            // 
+            // cbSicaklik
+            // 
+            this.cbSicaklik.FormattingEnabled = true;
+            this.cbSicaklik.Location = new System.Drawing.Point(534, 73);
+            this.cbSicaklik.Name = "cbSicaklik";
+            this.cbSicaklik.Size = new System.Drawing.Size(121, 21);
+            this.cbSicaklik.TabIndex = 1;
+            this.cbSicaklik.SelectedValueChanged += new System.EventHandler(this.cbSicaklik_SelectedValueChanged);
+            // 
+            // cbNem
+            // 
+            this.cbNem.FormattingEnabled = true;
+            this.cbNem.Location = new System.Drawing.Point(23, 152);
+            this.cbNem.Name = "cbNem";
+            this.cbNem.Size = new System.Drawing.Size(121, 21);
+            this.cbNem.TabIndex = 2;
+            this.cbNem.SelectedValueChanged += new System.EventHandler(this.cbNem_SelectedValueChanged);
+            // 
+            // cbRuzgar
+            // 
+            this.cbRuzgar.FormattingEnabled = true;
+            this.cbRuzgar.Location = new System.Drawing.Point(282, 73);
+            this.cbRuzgar.Name = "cbRuzgar";
+            this.cbRuzgar.Size = new System.Drawing.Size(121, 21);
+            this.cbRuzgar.TabIndex = 3;
+            this.cbRuzgar.SelectedValueChanged += new System.EventHandler(this.cbRuzgar_SelectedValueChanged);
+            // 
+            // cbSahaBoyutu
+            // 
+            this.cbSahaBoyutu.FormattingEnabled = true;
+            this.cbSahaBoyutu.Location = new System.Drawing.Point(23, 73);
+            this.cbSahaBoyutu.Name = "cbSahaBoyutu";
+            this.cbSahaBoyutu.Size = new System.Drawing.Size(121, 21);
+            this.cbSahaBoyutu.TabIndex = 4;
+            this.cbSahaBoyutu.SelectedValueChanged += new System.EventHandler(this.cbSahaBoyutu_SelectedValueChanged);
+            // 
+            // cbCimDurumu
+            // 
+            this.cbCimDurumu.FormattingEnabled = true;
+            this.cbCimDurumu.Location = new System.Drawing.Point(534, 152);
+            this.cbCimDurumu.Name = "cbCimDurumu";
+            this.cbCimDurumu.Size = new System.Drawing.Size(121, 21);
+            this.cbCimDurumu.TabIndex = 5;
+            this.cbCimDurumu.SelectedValueChanged += new System.EventHandler(this.cbCimDurumu_SelectedValueChanged);
+            // 
+            // lblFutbolOynamaDurumu
+            // 
+            this.lblFutbolOynamaDurumu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFutbolOynamaDurumu.AutoSize = true;
+            this.lblFutbolOynamaDurumu.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFutbolOynamaDurumu.ForeColor = System.Drawing.Color.Black;
+            this.lblFutbolOynamaDurumu.Location = new System.Drawing.Point(24, 356);
+            this.lblFutbolOynamaDurumu.Name = "lblFutbolOynamaDurumu";
+            this.lblFutbolOynamaDurumu.Size = new System.Drawing.Size(392, 32);
+            this.lblFutbolOynamaDurumu.TabIndex = 7;
+            this.lblFutbolOynamaDurumu.Text = "FUTBOL OYNAMA DURUMU:";
+            this.lblFutbolOynamaDurumu.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Location = new System.Drawing.Point(17, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(711, 41);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "FUTBOL OYNAMA DURUMU HESAPLAYICI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Saha Boyutu";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbCimDurumu);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbHavaDurumu);
+            this.groupBox1.Controls.Add(this.cbSahaBoyutu);
+            this.groupBox1.Controls.Add(this.cbNem);
+            this.groupBox1.Controls.Add(this.cbRuzgar);
+            this.groupBox1.Controls.Add(this.cbSicaklik);
+            this.groupBox1.Location = new System.Drawing.Point(24, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(704, 212);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seçimler";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(531, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 18);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Çim Durumu";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(279, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Hava Durumu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Nem";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(531, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Sıcaklık";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(279, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Rüzgar";
+            // 
+            // lblFutbolOyna
+            // 
+            this.lblFutbolOyna.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFutbolOyna.AutoSize = true;
+            this.lblFutbolOyna.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFutbolOyna.ForeColor = System.Drawing.Color.Green;
+            this.lblFutbolOyna.Location = new System.Drawing.Point(498, 356);
+            this.lblFutbolOyna.Name = "lblFutbolOyna";
+            this.lblFutbolOyna.Size = new System.Drawing.Size(205, 32);
+            this.lblFutbolOyna.TabIndex = 11;
+            this.lblFutbolOyna.Text = "OYNANABİLİR";
+            this.lblFutbolOyna.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(744, 425);
+            this.Controls.Add(this.lblFutbolOyna);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFutbolOynamaDurumu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Futbol Oyna";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbHavaDurumu;
+        private System.Windows.Forms.ComboBox cbSicaklik;
+        private System.Windows.Forms.ComboBox cbNem;
+        private System.Windows.Forms.ComboBox cbRuzgar;
+        private System.Windows.Forms.ComboBox cbSahaBoyutu;
+        private System.Windows.Forms.ComboBox cbCimDurumu;
+        private System.Windows.Forms.Label lblFutbolOynamaDurumu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFutbolOyna;
+    }
+}
+
---
+++ FutbolOyna_Agac_Odev/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FutbolOyna_Agac_Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetComboBoxes();
+        }
+
+        private void SetComboBoxes()
+        {
+            List<string> HavaDurumu = new List<string> { "Gunesli", "Bulutlu", "Yagmurlu" };
+            List<string> Sicaklik = new List<string> { "Sicak", "Ilik", "Soguk" };
+            List<string> Nem = new List<string> { "Normal", "Yuksek" };
+            List<string> Ruzgar = new List<string> { "Var", "Yok" };
+            List<string> SahaBoyutu = new List<string> { "Kucuk", "Orta", "Buyuk" };
+            List<string> CimDurumu = new List<string> { "Islak", "Kuru" };
+
+            cbHavaDurumu.DataSource = HavaDurumu;
+            cbSicaklik.DataSource = Sicaklik;
+            cbNem.DataSource = Nem;
+            cbRuzgar.DataSource = Ruzgar;
+            cbSahaBoyutu.DataSource = SahaBoyutu;
+            cbCimDurumu.DataSource = CimDurumu;
+
+            cbHavaDurumu.Enabled = false;
+            cbSicaklik.Enabled = false;
+            cbNem.Enabled = false;
+            cbRuzgar.Enabled = false;
+            cbCimDurumu.Enabled = false;
+        }
+
+        public void PrintResult(string result)
+        {
+            if (result == "Evet")
+            {
+                lblFutbolOyna.Text = "Oynanabilir";
+                lblFutbolOyna.ForeColor = Color.Green;
+            }
+            else if(result == "Hayir")
+            {
+                lblFutbolOyna.Text = "Oynanamaz";
+                lblFutbolOyna.ForeColor = Color.DarkRed;
+            }
+            else
+            {
+                lblFutbolOyna.Text = result;
+                lblFutbolOyna.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void ResetComboBoxes(ComboBox comboBox)
+        {
+            if(comboBox == cbSahaBoyutu)
+            {
+                cbRuzgar.Enabled = false;
+                cbSicaklik.Enabled = false;
+                cbNem.Enabled = false;
+                cbCimDurumu.Enabled = false;
+                cbHavaDurumu.Enabled = false;
+            }
+            else if(comboBox == cbRuzgar)
+            {
+                cbSicaklik.Enabled = false;
+                cbNem.Enabled = false;
+                cbCimDurumu.Enabled = false;
+                cbHavaDurumu.Enabled = false;
+            }
+            else if(comboBox == cbSicaklik)
+            {
+                cbNem.Enabled = false;
+                cbCimDurumu.Enabled = false;
+                cbHavaDurumu.Enabled = false;
+            }
+            else if(comboBox == cbNem)
+            {
+                cbCimDurumu.Enabled = false;
+            }
+            else if(comboBox == cbHavaDurumu)
+            {
+                cbCimDurumu.Enabled = false;
+            }
+        }
+
+        private void cbSahaBoyutu_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string value = (sender as ComboBox).SelectedItem as string;
+
+            if(value == "Buyuk")
+            {
+                PrintResult("Hayir");
+                ResetComboBoxes(sender as ComboBox);
+            }
+            else
+            {
+                PrintResult("?");
+                cbRuzgar.Enabled = true;
+            }
+        }
+
+        private void cbRuzgar_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string value = (sender as ComboBox).SelectedItem as string;
+
+            if ((cbSahaBoyutu.SelectedItem as string) == "Orta")
+            {
+                if (value == "Var")
+                {
+                    PrintResult("Hayir");
+                    ResetComboBoxes(sender as ComboBox);
+                }
+                else
+                {
+                    PrintResult("?");
+                    cbSicaklik.Enabled = true;
+                }
+            }
+            else
+            {
+                if (value == "Yok")
+                {
+                    PrintResult("Evet");
+                    ResetComboBoxes(sender as ComboBox);
+                }
+                else
+                {
+                    PrintResult("?");
+                    cbSicaklik.Enabled = true;
+                }
+            }
+        }
+
+        private void cbSicaklik_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string value = (sender as ComboBox).SelectedItem as string;
+
+            if (value == "Sicak")
+            {
+                PrintResult("Evet");
+                ResetComboBoxes(sender as ComboBox);
+            }
+            else if (value == "Soguk")
+            {
+                PrintResult("Hayir");
+                ResetComboBoxes(sender as ComboBox);
+            }
+            else
+            {
+                PrintResult("?");
+                if ((cbSahaBoyutu.SelectedItem as string) == "Orta")
+                    cbHavaDurumu.Enabled = true;
+                else
+                    cbNem.Enabled = true;
+            }
+        }
+
+        private void cbNem_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string value = (sender as ComboBox).SelectedItem as string;
+
+            if (value == "Yuksek")
+            {
+                PrintResult("Hayir");
+                ResetComboBoxes(sender as ComboBox);
+            }
+            else
+            {
+                PrintResult("?");
+                cbCimDurumu.Enabled = true;
+            }
+        }
+
+        private void cbHavaDurumu_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string value = (sender as ComboBox).SelectedItem as string;
+
+            if (value == "Bulutlu")
+            {
+                PrintResult("Evet");
+                ResetComboBoxes(sender as ComboBox);
+            }
+            else if (value == "Yagmurlu")
+            {
+                PrintResult("Hayir");
+                ResetComboBoxes(sender as ComboBox);
+            }
+            else
+            {
+                PrintResult("?");
+                cbCimDurumu.Enabled = true;
+            }
+        }
+
+        private void cbCimDurumu_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string value = (sender as ComboBox).SelectedItem as string;
+
+            if (value == "Kuru")
+                PrintResult("Evet");
+            else
+                PrintResult("Hayir");
+        }
+
+    }
+}
